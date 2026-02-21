2bd91bc8ab9125421438ac04a68731a281549721
giovanny.agama6161 <giovanny.agama6161@utc.edu.ec>
2026-02-20 21:39:40 -0500

    primera parte funcional

--- gestion/src/main/java/com/utc/gestion/entity/Usuario.java
+++ gestion/src/main/java/com/utc/gestion/entity/Usuario.java
@@ -20,5 +20,37 @@ public class Usuario {
     @Column(name = "role")
     private String role;
 
-    // getters y setters
+    // ===== GETTERS Y SETTERS =====
+
+    public Long getCodigoUsu() {
+        return codigoUsu;
+    }
+
+    public void setCodigoUsu(Long codigoUsu) {
+        this.codigoUsu = codigoUsu;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
